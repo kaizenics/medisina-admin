6999046dea5e29d6114277e8f19a73d1db4705d9
kaizenics <nicosejohnsoriano@gmail.com>
2024-04-10 21:04:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template >
+ <main>
+  <h2 class="text-red-600 text-3xl">Test App</h2>
+ </main>
+</template>
+
