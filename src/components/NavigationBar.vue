0f22129cd5a7bd31ad0c55a0ee951dbf8ad40841
kaizenics <nicosejohnsoriano@gmail.com>
2024-04-10 22:44:56 +0800

    refactor: NavigationBar.vue and ModeToggle.vue components

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-
+import ModeToggle from '@/components/ui/ModeToggle.vue'
 </script>
 
 <template>
     <nav class="container flex flex-col">
         <div class="my-7">
             <ul class="flex justify-between items-center">
-                <li class="text-slate-700 font-montserrat font-bold text-4xl"><a href="#">Medisina</a></li>
+                <li class="text-slate-700 dark:text-slate-100 font-montserrat font-bold text-4xl"><a
+                        href="#">Medisina</a></li>
                 <ul class="font-montserrat text-md flex space-x-10">
                     <li><a href="#">Our Experts</a></li>
                     <li><a href="#">How it works</a></li>
@@ -15,6 +16,9 @@
                 <ul class="flex space-x-4">
                     <li class="font-monserrat font-semibold text-lg py-3"><a href="#">Login</a></li>
                     <button class="font-montserrat text-md bg-slate-800 text-white py-3 px-4">Sign up for free</button>
+                    <div class="py-2">
+                        <ModeToggle />
+                    </div>
                 </ul>
             </ul>
         </div>
--- src/components/ui/ModeToggle.vue
+++ src/components/ui/ModeToggle.vue
@@ -1,13 +1,33 @@
-<script>
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+
+const mode = useColorMode()
 </script>
 
 <template>
-    <div class="flex flex-col items-center">
-        <div class="flex flex-col items-center">
-            <h1 class="font-montserrat text-5xl font-bold text-slate-800">Welcome to Medisina</h1>
-            <p class="font-montserrat text-lg text-slate-700">Your health is our priority</p>
-        </div>
-        <div class="flex flex-col items-center">
-            <img src="../assets/hero.png" alt="hero" class="w-96 h-96" />
-        </div>
-    </div>
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+            <Button variant="outline">
+                <Icon icon="radix-icons:moon"
+                    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Icon icon="radix-icons:sun"
+                    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span class="sr-only">Toggle theme</span>
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuItem @click="mode = 'light'">
+                Light
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'dark'">
+                Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'auto'">
+                System
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
