58aee601f3471fbbfab67a509f6819db0174622d
kaizenics <nicosejohnsoriano@gmail.com>
2024-04-10 22:31:10 +0800

    feat: mode toggle for dark mode added

---
+++ src/components/ui/ModeToggle.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center">
+            <h1 class="font-montserrat text-5xl font-bold text-slate-800">Welcome to Medisina</h1>
+            <p class="font-montserrat text-lg text-slate-700">Your health is our priority</p>
+        </div>
+        <div class="flex flex-col items-center">
+            <img src="../assets/hero.png" alt="hero" class="w-96 h-96" />
+        </div>
+    </div>
