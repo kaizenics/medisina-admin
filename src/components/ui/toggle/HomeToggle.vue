6aab380376d311b145365523fa4945ea14f2053d
kaizenics <nicosejohnsoriano@gmail.com>
2024-04-21 15:54:10 +0800

    feat: hometoggle updated

--- src/components/ui/toggle/HomeToggle.vue
+++ src/components/ui/toggle/HomeToggle.vue
@@ -9,7 +9,7 @@ const mode = useColorMode()
 
 <template>
     <DropdownMenu>
-        <DropdownMenuTrigger as-child class="flex py-4">
+        <DropdownMenuTrigger as-child class="flex py-4 cursor-pointer">
             <div class="border-none bg-transparent">
                 <Icon icon="radix-icons:moon"
                     class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-white" />
@@ -19,13 +19,13 @@ const mode = useColorMode()
             </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-            <DropdownMenuItem @click="mode = 'light'">
+            <DropdownMenuItem @click="mode = 'light'" class="cursor-pointer">
                 Light
             </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'dark'">
+            <DropdownMenuItem @click="mode = 'dark'" class="cursor-pointer">
                 Dark
             </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'auto'">
+            <DropdownMenuItem @click="mode = 'auto'" class="cursor-pointer">
                 System
             </DropdownMenuItem>
         </DropdownMenuContent>
