a7f2a7b9bda6712ce14cd487299d37588f5c99b5
kaizenics <nicosejohnsoriano@gmail.com>
2024-04-21 15:55:06 +0800

    styles: home page styles updated

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Autoplay from 'embla-carousel-autoplay'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+import { Card, CardContent } from '@/components/ui/card'
+import poster1 from '@/assets/images/drugstore-poster.jpg'
+import poster2 from '@/assets/images/drugstore-poster2.jpg'
+import poster3 from '@/assets/images/drugstore-poster3.jpg'
+import poster4 from '@/assets/images/drugstore-poster4.jpg'
+import poster5 from '@/assets/images/drugstore-poster5.jpg'
+
+const plugin = Autoplay({
+    delay: 2000,
+    stopOnMouseEnter: true,
+    stopOnInteraction: false,
+})
+
+const posters = [poster1, poster2, poster3, poster4, poster5]
+</script>
+
+<template>
+    <main>
+        <Carousel class="relative w-full max-w-12xl" :plugins="[plugin]" @mouseenter="plugin.stop"
+            @mouseleave="[plugin.reset(), plugin.play(), console.log('Runing')];">
+            <CarouselContent>
+                <CarouselItem v-for="(poster, index) in posters" :key="index" class="">
+                    <div class="p-1">
+                        <Card>
+                            <CardContent class="flex aspect-auto items-center justify-center p-1 ">
+                                <img :src="poster" alt="Poster Image" class="object-cover w-full h-[500px]">
+                            </CardContent>
+                        </Card>
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </main>
+</template>
--- src/components/HomeNavbar.vue
+++ src/components/HomeNavbar.vue
@@ -4,24 +4,29 @@ import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-    <nav class="bg-gray-950 dark:bg-gray-950">
-        <div class="font-montserrat container mx-auto px-0 flex flex-col">
-            <div class="my-4">
+    <nav class="bg-gray-950">
+        <div class="font-montserrat container mx-auto px-0 py-4 flex flex-col">
+            <div class="my-1">
                 <ul class="flex justify-between items-center">
                     <li class="text-emerald-500 font-montserrat font-bold text-4xl"><a href="/">Medisina</a></li>
 
-                    <ul class="flex justify-end">
+                    <ul class="flex flex-col justify-end pt-5">
                         <div class="relative">
                             <input type="text" placeholder="Search for Brand or Generic Medicine"
                                 class="outline-none py-2 px-5 text-gray-900 w-[600px] h-[40px]">
-                            <button class="absolute bg-gray-900 py-2 px-10 top-0 right-0 text-gray-200">Search</button>
+                            <button
+                                class="absolute bg-gray-900 py-2 px-5 top-0 right-0 text-gray-200 hover:bg-gray-800 transition-colors duration-300">Search</button>
                         </div>
-
-
+                        <h1 class="text-sm text-white">Keywords: Medisina Vitamin E or Guardian Goats Milk
+                        </h1>
                     </ul>
                     <ul class="flex space-x-4">
-                        <div class="font-monserrat font-semibold text-lg py-3 cursor-pointer">
-                            <Icon icon="mdi:cart" class="h-[1.8rem] w-[1.8rem] text-white" />
+                        <div class="bg-gray-800 w-[110px] h-[40px] m-auto rounded-sm py-3 cursor-pointer">
+                            <div class="flex justify-center space-x-3">
+
+                                <Icon icon="bi:cart" class="h-[1.2rem] w-[1.2rem] text-white" />
+                                <p class="font-semibold text-white text-sm">0 item/s</p>
+                            </div>
                         </div>
                         <div class="font-monserrat font-semibold text-lg py-3 cursor-pointer">
                             <Icon icon="ant-design:menu-outlined" class="h-[1.8rem] w-[1.8rem] text-white" />
