4462e8bd7ede81a956a6d0524215cdacf8ff7b2f
kaizenics <nicosejohnsoriano@gmail.com>
2024-05-13 16:22:20 +0800

    refactor: Update OverviewBar component to use dynamic data for monthly totals

--- src/components/dashboard/OverviewBar.vue
+++ src/components/dashboard/OverviewBar.vue
@@ -1,32 +1,31 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { VisAxis, VisStackedBar, VisXYContainer } from '@unovis/vue';
 
+type Data = {
+    name: string;
+    total: number;
+};
 
-import { VisAxis, VisStackedBar, VisXYContainer } from '@unovis/vue'
+const data = ref<Data[]>([]);
 
-type Data = typeof data[number]
-const data = [
-    { name: 'Jan', total: Math.floor(Math.random() * 5000) + 0 },
-    { name: 'Feb', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Mar', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Apr', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'May', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Jun', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Jul', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Aug', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Sep', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Oct', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Nov', total: Math.floor(Math.random() * 5000) + 1000 },
-    { name: 'Dec', total: Math.floor(Math.random() * 5000) + 1000 },
-]
+onMounted(() => {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const monthlyTotals = months.map((month) => ({
+        name: month,
+        total: Math.floor(Math.random() * 1000) + 1,
+    }));
+
+    data.value = monthlyTotals;
+});
 </script>
 
 <template>
     <VisXYContainer height="350px" :margin="{ left: 20, right: 20 }" :data="data">
         <VisStackedBar :x="(d: Data, i: number) => i" :y="(d: Data) => d.total" color="#41b883" :rounded-corners="4"
             :bar-padding="0.15" />
-        <VisAxis type="x" :num-ticks="data.length" :tick-format="(index: number) => data[index]?.name"
-            :grid-line="false" :tick-line="false" color="#888888" />
-        <VisAxis type="y" :num-ticks="data.length" :tick-format="(index: number) => data[index]?.name"
-            :grid-line="false" :tick-line="false" :domain-line="false" color="#888888" />
+        <VisAxis type="x" :num-ticks="data.length" :tick-format="(i: number) => data[i]?.name" :grid-line="false"
+            :tick-line="false" color="#888888" />
+        <VisAxis type="y" :num-ticks="5" :grid-line="false" :tick-line="false" color="#888888" />
     </VisXYContainer>
 </template>
